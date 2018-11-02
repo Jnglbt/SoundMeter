7ceadc23e03603599358e481c052ce40f878ab30
Jnglbt <anton.chepov@gmail.com>
2018-11-02 23:42:38 +0100

    Measure activity

--- app/src/main/java/com/soundmeterpl/MeasureActivity.java
+++ app/src/main/java/com/soundmeterpl/MeasureActivity.java
@@ -1,7 +1,10 @@
 package com.soundmeterpl;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MeasureActivity extends Activity
 {
@@ -11,5 +14,16 @@ public class MeasureActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measure);
+
+        Button abort = (Button) findViewById(R.id.abort_bttn);
+        abort.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MeasureActivity.this, Main2Activity.class));
+            }
+        });
     }
 }
